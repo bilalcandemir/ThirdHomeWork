877597067603a7ce6c6d415c5868b07489b01a0c
Bilal Candemir <candemirbilal@yahoo.com>
2022-09-18 21:13:38 +0300

    Initial Commit

---
+++ ThirdHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdHomeWork
+//
+//  Created by Bilal Candemir on 9/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
